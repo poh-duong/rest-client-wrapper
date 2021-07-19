a4dc4f82f6bcf942cacf4c40eeb25a5a652ec5a6
Poh Duong <poh.duong@adelaide.edu.au>
2021-07-20 09:05:14 +0930

    TAPP-2293 - Fix issue of json parsing empty response body

--- lib/rest_client_wrapper/rest_client.rb
+++ lib/rest_client_wrapper/rest_client.rb
@@ -153,8 +153,8 @@ module RestClientWrapper
     end
 
     def _parse_json(response)
+      return response.body if response.body&.empty?
       return { ok: true } if response.body == "ok".to_json # Handle special case for Echo delete responses
-
       return JSON.parse(response.body, { object_class: Hash, symbolize_names: true })
     rescue StandardError => e
       raise RestClientError.new("Response could not be parsed as JSON", response, e)
