d84529bf6b14bfefeb910bd578295a52e5823dd8
Poh Duong <poh.duong@adelaide.edu.au>
2021-07-21 12:16:32 +0930

    TAPP-2293

--- lib/rest_client_wrapper/request.rb
+++ lib/rest_client_wrapper/request.rb
@@ -30,11 +30,11 @@ module RestClientWrapper
 
     def initialize(**params)
       @uri = uri
-      self.headers = params[:headers]
+      self.headers = params[:headers].nil? ? {} : params[:headers]
       self.http_method = params[:http_method]
-      self.segment_params = params[:segment_params]
-      self.payload = params[:payload]
-      self.query_params = params[:query_params]
+      self.segment_params = params[:segment_params].nil? ? {} : params[:segment_params] 
+      self.payload = params[:payload].nil? ? {} : params[:payload] 
+      self.query_params = params[:query_params].nil? ? {} : params[:query_params] 
     end
 
     def http_method=(http_method)
